3c0c6579720c06d57d4dc0cb0b96cfed633bee08
Amyxue <632032016@qq.com>
2019-08-10 11:01:33 +0800

    薛周考

---
+++ 薛岭钰zk-2/data.js
@@ -0,0 +1,7 @@
+var data = [
+    { value: 335, name: '直接访问', color: "rgb(213, 58, 53)" },
+    { value: 310, name: '邮件营销', color: "rgb(51, 75, 92)" },
+    { value: 234, name: '联盟广告', color: "rgb(106, 176, 184)" },
+    { value: 135, name: '视频广告', color: "rgb(233, 143, 111)" },
+    { value: 1548, name: '搜索引擎', color: "rgb(145, 199, 174)" }
+]
